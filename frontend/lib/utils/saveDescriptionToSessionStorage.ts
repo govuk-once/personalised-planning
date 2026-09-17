c36b4a829b8934d16188a0c37a0b289d44234926
Umar Hansa <umar.hansa@digital.cabinet-office.gov.uk>
2026-09-17 14:33:12 +0100

    Remove old chat template
    Deletes the starter chat UI and helper files we no longer use.

--- frontend/lib/utils/saveDescriptionToSessionStorage.ts
+++
@@ -1,9 +0,0 @@
-export function saveDescriptionToSessionStorage(item: any) {
-  const itemName = "description";
-
-  if(sessionStorage.getItem(itemName)) {
-    sessionStorage.clear();
-  }
-  
-  sessionStorage.setItem(itemName, JSON.stringify(item));
-}
